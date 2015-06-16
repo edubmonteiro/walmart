036bfbc7a6318aba411859d8a1ea554317c4522d
Eduardo Monteiro <edubmonteiro@gmail.com>
2015-06-15 23:36:46 -0300

    Update search_definitions.js

--- Features/Feature/step_definitions/search_definitions.js
+++ Features/Feature/step_definitions/search_definitions.js
@@ -1,10 +1,10 @@
-'use strict';
+\\'use strict';
 
-var searchDefinitions = function () {
-  this.World = require('../support/world.js').World;
-  this.Given(/^visito a página de busca$/, function (callback) {
-    this.browser.visitPath('/v1/VejaSP/search', callback);
-  });
-};
+\\var searchDefinitions = function () {
+\\  this.World = require('../support/world.js').World;
+\\  this.Given(/^visito a página de busca$/, function (callback) {
+\\    this.browser.visitPath('/v1/VejaSP/search', callback);
+\\  });
+\\};
 
-module.exports = searchDefinitions;
+\\module.exports = searchDefinitions;
